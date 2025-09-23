42b6c41e4e36c7d2e5a5317b20065a27b6ec8028
sindhuaccet07 <sindhuaccet07@gmail.com>
2025-09-23 15:23:59 -0400

    deleted the old test file

--- src/test/java/pages/RecipeListingPage.java
+++ src/test/java/pages/RecipeListingPage.java
@@ -14,9 +14,6 @@ public class RecipeListingPage {
     @FindBy(xpath = ".//h5/a")
     private List<WebElement> recipeLinks;
 
-//    @FindBy(xpath = "//a[contains(text(),'Next') and contains(@class,'page-link')]")
-//    private List<WebElement> nextButtons;
-
     private final By recipeLinkLocator = By.xpath(".//h5/a");
     private final By nextBtnLocator = By.xpath("//a[contains(text(),'Next') and contains(@class,'page-link')]");
 
@@ -25,27 +22,6 @@ public class RecipeListingPage {
         this.elementsUtil = new ElementsUtil(driver);
         PageFactory.initElements(driver, this);
     }
-
-    // Get all recipe URLs on the current page that haven't been visited
-//    public List<String> getRecipeUrls(List<String> visitedRecipes) {
-//        wait.until(ExpectedConditions.presenceOfAllElementsLocatedBy(By.xpath(".//h5/a")));
-//        return recipeLinks.stream()
-//                .map(e -> e.getAttribute("href"))
-//                .filter(url -> !visitedRecipes.contains(url))
-//                .collect(Collectors.toList());
-//    }
-
-    // Navigate to next page if available
-//    public boolean goToNextPage() {
-//        if (!nextButtons.isEmpty()) {
-//            WebElement nextBtn = nextButtons.get(0);
-//            ((JavascriptExecutor) driver).executeScript("arguments[0].scrollIntoView(true);", nextBtn);
-//            ((JavascriptExecutor) driver).executeScript("arguments[0].click();", nextBtn);
-//            wait.until(ExpectedConditions.stalenessOf(nextBtn));
-//            return true;
-//        }
-//        return false;
-//    }
 
     public List<String> getRecipeUrls(List<String> visitedRecipes) {
         try {
@@ -60,7 +36,6 @@ public class RecipeListingPage {
         }
     }
 
-    
     public boolean goToNextPage() {
         try {
             if (!elementsUtil.isElementDisplayed(nextBtnLocator)) {
